73c68bd454f6e9f4f22c34a04d5ddc27e100e25b
Alexander Zarubin <whitesashka@gmail.com>
2024-08-12 18:13:51 +0500

    add 006_mutex

---
+++ 006_mutex/main.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<mutex>
+#include<thread>
+#include"Timer.h"
+using namespace std;
+
+/*
+	Многопоточное програмирование
+
+		      mutex
+
+	 защита разделяемых данных
+
+       синхронизация потоков
+*/
+mutex mtx;
+void Print(char ch)
+{
+	this_thread::sleep_for(chrono::milliseconds(2000));
+	mtx.lock();
+	cout << "id потока =  " << this_thread::get_id()<<"\tstart" << endl;
+	for (size_t i = 0; i < 5; i++)
+	{
+		for (size_t i = 0; i < 10; i++)
+		{
+			cout << ch;
+			this_thread::sleep_for(chrono::milliseconds(20));
+		}
+		cout << endl;
+	}
+	cout << "id потока =  " << this_thread::get_id()<<"\tstop\n" << endl;
+	mtx.unlock();
+	this_thread::sleep_for(chrono::milliseconds(2000));
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	Timer timer;
+	thread t1(Print, '*');
+	thread t2(Print, '#');
+	
+	t1.join();
+	t2.join();
+	/*Print('*');
+	Print('#');*/
+
+	return 0;
+}
